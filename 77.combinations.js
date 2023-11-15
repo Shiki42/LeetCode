de4e4bda782510738d528996c0a01051191955ea
Shuyuan <kukutou_mikiya@outlook.com>
2023-11-14 23:08:45 -0500

    77 done

--- 77.combinations.js
+++ 77.combinations.js
@@ -10,8 +10,20 @@
  * @param {number} k
  * @return {number[][]}
  */
-var combine = function(n, k) {
-    
+var combine = function (n, k) {
+  function backtrack(start, cur) {
+    if (cur.length === k) {
+      res.push(cur);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      if (k - cur.length <= n - i + 1) {
+        backtrack(i + 1, [...cur, i]);
+      }
+    }
+  }
+  let res = [];
+  backtrack(1, []);
+  return res;
 };
 // @lc code=end
-
