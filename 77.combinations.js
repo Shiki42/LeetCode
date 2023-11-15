afc0662686d24c96d7086d0eeb005151ac7369b1
Shuyuan <kukutou_mikiya@outlook.com>
2023-11-14 23:16:28 -0500

    77 update, always push and pop in backtracking

--- 77.combinations.js
+++ 77.combinations.js
@@ -10,6 +10,26 @@
  * @param {number} k
  * @return {number[][]}
  */
+//  The right way is push and pop on the same array, so no cost of copying array.
+var combine = function (n, k) {
+  function backtrack(start, cur) {
+    if (cur.length === k) {
+      res.push([...cur]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      if (k - cur.length <= n - i + 1) {
+        cur.push(i);
+        backtrack(i + 1, cur);
+        cur.pop();
+      }
+    }
+  }
+  let res = [];
+  backtrack(1, []);
+  return res;
+};
+// The wrong way
 var combine = function (n, k) {
   function backtrack(start, cur) {
     if (cur.length === k) {
