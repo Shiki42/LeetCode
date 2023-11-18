29e18b9bebad78c2c17d1d19ea21f47b51586470
Shuyuan <kukutou_mikiya@outlook.com>
2023-11-17 23:57:23 -0500

    276

---
+++ 276.paint-fence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var numWays = function (n, k) {
+  let memo = {};
+  let total_way = (i) => {
+    if (i === 1) return k;
+    if (i === 2) return k * k;
+    if (memo[i]) return memo[i];
+    memo[i] = total_way(i - 1) * (k - 1) + total_way(i - 2) * (k - 1);
+    return memo[i];
+  };
+};
