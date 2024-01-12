51416c599c255ab4735042c48e4abe60b42c25dc
Shuyuan <kukutou_mikiya@outlook.com>
2024-01-11 23:57:57 -0500

    110 done

---
+++ 110. Balanced Binary Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        def dfs(root,depth):
+            if not root:
+                return depth
+            else:
+                left = dfs(root.left,depth+1)
+                right = dfs(root.right,depth+1)
+                print(left,right)
+                if left == -1 or right == -1 or abs(left-right) > 1:
+                    return -1
+                else:
+                    return depth
+        if not root:
+            return True
+        return dfs(root,0) != -1
