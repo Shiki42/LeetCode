fa298a26f14016d7c51c6d3797feb7e0d56b7777
Shuyuan <kukutou_mikiya@outlook.com>
2024-02-25 15:45:03 -0500

    815 done

--- 815. Bus Routes.py
+++ 815. Bus Routes.py
@@ -3,21 +3,27 @@ class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
       if source == target:
         return 0
-      q = [source]
-      deep_map = {source:0}
-      routes = list(map(set, routes))
+      q = []
+      deep_map = {}
+      routes = {i: route for i, route in enumerate(map(set, routes))}
+      for bus in routes:
+          if source in routes[bus]:
+              deep_map[bus] = 1
+              q.append(bus)
+
       res = 501
+
       while q:
         cur = q.pop(0)
         depth = deep_map[cur]
+        if target in routes[cur]:
+            return depth
         for bus in routes:
-            if cur in bus:
-                for st in bus:
-                    if st == target:
-                        res = min(depth + 1, res)
-                    elif st not in deep_map or deep_map[st] > depth + 1:
-                        deep_map[st] = depth + 1
-                        q.append(st)
+            for st in routes[cur]:
+                if st in routes[bus] and bus not in deep_map:
+                    deep_map[bus] = depth + 1
+                    q.append(bus)
       if res == 501:
           return -1
       return res
+
