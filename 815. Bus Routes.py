4c08902bb20a4d53d32b5dcdd19daa53e9f89405
Shuyuan <kukutou_mikiya@outlook.com>
2024-02-24 23:09:53 -0500

    815 underwork

--- 815. Bus Routes.py
+++ 815. Bus Routes.py
@@ -1,33 +1,23 @@
 from collections import defaultdict
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
-      q = []
-      deep_map = {source:1}
-      routes = map(set, routes)
       if source == target:
         return 0
-      for i in routes[::-1]:
-        if source in i:
-          for j in i:
-            if j == target:
-              return 1
-            if j not in deep_map:
-              deep_map[j] = 1
-              q.append(j)
-    
+      q = [source]
+      deep_map = {source:0}
+      routes = list(map(set, routes))
+      res = 501
       while q:
-        new = []
-        while q:
-          cur = q.pop()
-          for i in routes[::-1]:
-            if cur in i:
-              for j in i:
-                if j == target:
-                  return deep_map[cur]+1
-                if j not in deep_map:
-                  deep_map[j] = deep_map[cur]+1
-                  new.append(j)
-              routes.remove(i)
-        q = new
-        
-      return -1
+        cur = q.pop(0)
+        depth = deep_map[cur]
+        for bus in routes:
+            if cur in bus:
+                for st in bus:
+                    if st == target:
+                        res = min(depth + 1, res)
+                    elif st not in deep_map or deep_map[st] > depth + 1:
+                        deep_map[st] = depth + 1
+                        q.append(st)
+      if res == 501:
+          return -1
+      return res
