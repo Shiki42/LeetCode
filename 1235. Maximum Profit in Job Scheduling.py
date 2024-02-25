7b34409289e985adae7919b28df62d4a104b6183
Shuyuan <kukutou_mikiya@outlook.com>
2024-02-24 19:11:20 -0500

    1235

--- 1235. Maximum Profit in Job Scheduling.py
+++ 1235. Maximum Profit in Job Scheduling.py
@@ -1,5 +1,23 @@
 class Solution:
-    def findMaxProfit(jobs):
-        return
+    def findMaxProfit(self, i, jobs, startTime, dp, n):
+        if i == n:
+            return 0
+        if dp[i] != -1:
+            return dp[i]
+
+        index = bisect.bisect_left(startTime,jobs[i][1])
+
+        pick = jobs[i][2] + self.findMaxProfit(index, jobs, startTime, dp, n)
+        nonpick = self.findMaxProfit(i + 1, jobs, startTime, dp, n)
+        dp[i] = max(pick,nonpick)
+        return dp[i]
+        
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        return
+        n = len(profit)
+        jobs = [[startTime[i], endTime[i], profit[i]] for i in range(n)]
+        dp = [-1 for _ in range(n)]
+
+        jobs.sort(key=lambda x:x[0])
+        startTime.sort()
+
+        return self.findMaxProfit(0, jobs, startTime, dp, n)
