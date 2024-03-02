5657ad95f680c2fc2d3997f7fc825f9a29825748
Shuyuan <kukutou_mikiya@outlook.com>
2024-03-02 00:01:34 -0500

    underwork

--- 2781. Length of the Longest Valid Substring.py
+++ 2781. Length of the Longest Valid Substring.py
@@ -1,4 +1,18 @@
 class Solution:
     def longestValidSubstring(self, word: str, forbidden: List[str]) -> int:
+        n = len(word)
         forbidden_set = set(forbidden)
         res = 0
+        # memo = [-1 for _ in range(n)]
+        # memo[0] = 1
+        memo = 1 if word[0] not in forbidden_set else 0
+        for i in range(1, n):
+            cur = word[i-memo:i+1]
+            k = memo
+            for j in range(k):
+                if cur[k-j-1:k+1] in forbidden_set:
+                    res = max(memo,res)
+                    memo = j-1
+            if k == memo:
+                memo += 1
+        return max(memo,res)
