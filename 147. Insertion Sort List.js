8849bc9dc6bb5098357ce3c305229887b467c818
Shuyuan <kukutou_mikiya@outlook.com>
2024-04-25 15:07:01 -0400

    147

---
+++ 147. Insertion Sort List.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+    if (!head || !head.next) return head;
+
+    let nodes = [];
+    while (head) {
+        nodes.push(head);
+        head = head.next;
+    }
+
+    nodes.sort((a, b) => a.val - b.val); 
+
+    for (let i = 0; i < nodes.length - 1; i++) {
+        nodes[i].next = nodes[i + 1];
+    }
+    nodes[nodes.length - 1].next = null; 
+
+    return nodes[0]; 
+};
