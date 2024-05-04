801d27be35aaf5ff64781c962466e5cc4e303aa3
Shuyuan <kukutou_mikiya@outlook.com>
2024-05-03 23:28:18 -0400

    java 90

---
+++ 90. Subsets II.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<> ();
+        result.add(new ArrayList<>());
+        for (int i = 0; i < nums.length; ++i) {
+            List<List<Integer>> newResult = new ArrayList<>();
+            for (List<Integer> L: result) {
+                L = new ArrayList<>(L);
+                L.add(nums[i]);
+                newResult.add(L);
+            }
+            result.addAll(newResult);
+        }
+        return result;
+    }
+}
